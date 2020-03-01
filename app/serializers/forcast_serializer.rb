c7ffc9f96678ceecd594420992fabc692536268b
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-01 13:26:06 -0700

    Add method for fetching today and next 5 days weather

--- app/serializers/forcast_serializer.rb
+++ app/serializers/forcast_serializer.rb
@@ -2,12 +2,12 @@ class ForcastSerializer
   include FastJsonapi::ObjectSerializer
 
   def self.generate_json(forcast)
-    require "pry"; binding.pry
+    #five_day_forcast returns today's weather as well as the next 5 days
     response = {
       location: forcast.location,
       weather: {
         current_weather: forcast.forcast_response.current_weather,
-        
+        five_day_forcast: forcast.forcast_response.daily_forcast
       }
     }
   end
