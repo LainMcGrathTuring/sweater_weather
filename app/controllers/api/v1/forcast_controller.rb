644e930367a3baae06aeca9e562f7a58be88b57c
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-01 12:22:52 -0700

    Add serializer

--- app/controllers/api/v1/forcast_controller.rb
+++ app/controllers/api/v1/forcast_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ForcastController < ApplicationController
 
   def index
    forcast = ForcastFacade.new(params)
-   require "pry"; binding.pry
-   render json: forcast
+
+   render json: forcast.forcast_serializer
   end
 end
