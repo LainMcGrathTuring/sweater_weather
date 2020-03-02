cbd8abc955ddacf1039bb05428f8c94b78410e8d
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-02 10:02:52 -0700

    add distance call

--- app/services/location_fetcher.rb
+++ app/services/location_fetcher.rb
@@ -7,14 +7,32 @@ class LocationFetcher
   def get_location(params)
     conn = connection
     response = conn.get("maps/api/geocode/json") do |req|
-      req.params['address'] = params[:location]
+      req.params['address'] = "#{params}"
       req.params['key'] = ENV['GOOGLE_API']
     end
-    response_parse(response)
+    location_parse(response)
   end
 
-  def response_parse(response)
+  def location_parse(response)
     parse = JSON.parse(response.body, symbolize_names: true)
     Location.save_location(parse)
+  end
+
+  def get_distance(start, destination)
+    conn = connection
+    # response = conn.get("maps/api/directions/json") do |req|
+    #   req.params['origin'] = "#{start.latitude}","#{start.longitude}"
+    #   req.params['destination'] = "#{destination.latitude}","#{destination.longitude}"
+    #   req.params['key'] = ENV['GOOGLE_API']
+    # end
+
+    response = conn.get("maps/api/directions/json?origin=#{start.latitude},#{start.longitude}
+                        &destination=#{destination.latitude},#{destination.longitude}&key=#{ENV['GOOGLE_API']}")
+    distance_parse(response)
+  end
+
+  def distance_parse(response)
+    parse = JSON.parse(response.body, symbolize_names: true)
+    Trip.save_trip(parse)
   end
 end
