103449197d298b0808689f79b2aa03edd99eb5b7
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-01 18:24:53 -0700

    add image

---
+++ app/services/image_fetcher.rb
@@ -0,0 +1,19 @@
+class ImageFetcher
+
+  def connection
+    Faraday.new("https://api.unsplash.com")
+  end
+
+  def get_image(location)
+    conn = connection
+    response = conn.get("search/photos") do |req|
+      req.params['client_id'] = ENV['UPSPLASH_ACCESS_KEY']
+      req.params['query'] = "#{location.city}, #{location.state} skyline"
+    end
+    parse_response(response)
+  end
+
+  def parse_response(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
