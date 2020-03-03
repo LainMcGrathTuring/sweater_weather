bf41b5297f924d6edc744ffd0b7379d3179ba22a
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-03 15:17:12 -0700

    remove extra files from final_assessment

--- app/controllers/api/v1/munchies_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::MunchiesController < ApplicationController
-
-  def index
-    facade = YelpFacade.new(params)
-    render json: facade.yelp_serializer
-  end
-end
--- app/poros/restaurant_summary.rb
+++
@@ -1,9 +0,0 @@
-class RestaurantSummary
-
-  def self.restaurants_summary(params)
-    return @restaurant if @restaurant
-    @restaurant = YelpFetcher.new.get_business(params)
-    Restaurant.new(@restaurant)
-  end
-
-end
