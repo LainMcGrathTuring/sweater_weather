ff9ea9dec57c5bba53451649870fd6bce7c98104
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-03 22:51:50 -0700

    Add distance method

--- app/services/location_fetcher.rb
+++ app/services/location_fetcher.rb
@@ -16,4 +16,18 @@ class LocationFetcher
     parse = JSON.parse(response.body, symbolize_names: true)
     Location.save_location(parse)
   end
+
+  def get_distance(origin, destination)
+    response = conn.get("maps/api/directions/json") do |req|
+      req.params['origin'] = "#{origin.latitude}, #{origin.longitude}"
+      req.params['destination'] = "#{destination.latitude}, #{destination.longitude}"
+      req.params['key'] = ENV['GOOGLE_API']
+    end
+    distance_parse(response)
+  end
+
+  def distance_parse(response)
+    parse = JSON.parse(response.body, symbolize_names: true)
+    Trip.new(parse[:routes][0][:legs][0])
+  end
 end
