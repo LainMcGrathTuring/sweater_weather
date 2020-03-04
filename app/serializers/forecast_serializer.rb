e36e42bb58ec8c7e3ef5e6e674376bf34f46be05
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-03 19:20:10 -0700

    refactor forecast_facade

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,17 +1,7 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-
-  def self.generate_json(forecast)
-    #five_day_forcast returns today's weather as well as the next 5 days
-    #hourly_forcast returns the current hour's weather as well as the next 7 hours
-    response = {
-      location: forecast.location_results,
-      image: forecast.image,
-      weather: {
-        current_weather: forecast.forecast_response.current_weather,
-        hourly_forcast: forecast.forecast_response.hourly_forecast,
-        five_day_forcast: forecast.forecast_response.daily_forecast
-      }
-    }
-  end
+    attributes :location_results,
+               :daily_weather_forecast,
+               :hourly_weather_forecast,
+               :current_weather
 end
