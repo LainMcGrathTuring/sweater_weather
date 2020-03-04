9b7bad8c8e53fbc7fb2acc2fda131e7bd02afb55
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-03 19:27:36 -0700

    remove comment

--- app/services/location_fetcher.rb
+++ app/services/location_fetcher.rb
@@ -20,15 +20,6 @@ class LocationFetcher
 
   def get_distance(start, destination)
     conn = connection
-    #could not get this to recognize that I was passing an origin params to work,
-    #wrote out url for the sake of time
-
-    # response = conn.get("maps/api/directions/json") do |req|
-    #   req.params['origin'] = "#{start.latitude}","#{start.longitude}"
-    #   req.params['destination'] = "#{destination.latitude}","#{destination.longitude}"
-    #   req.params['key'] = ENV['GOOGLE_API']
-    # end
-
     response = conn.get("maps/api/directions/json?origin=#{start.latitude},#{start.longitude}
                         &destination=#{destination.latitude},#{destination.longitude}&key=#{ENV['GOOGLE_API']}")
     distance_parse(response)
