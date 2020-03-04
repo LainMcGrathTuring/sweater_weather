a728683442e305f822e5ebeaa0e393313a64f12d
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-03 20:36:06 -0700

    disable vcr

--- spec/requests/api/v1/image/location_image_spec.rb
+++ spec/requests/api/v1/image/location_image_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Image API', :vcr => { :re_record_interval => 7.days } do
+RSpec.describe 'Image API' do
   it 'sends an image of the location searched for' do
+    VCR.turn_off! :ignore_cassettes => true
+    WebMock.allow_net_connect!
+    
     location = Location.create(id: 143, city: "Denver", state: "Colorado", country: "United States", latitude: 39.7392358, longitude: -104.87)
 
     get "/api/v1/backgrounds?location=Denver,Colorado"
