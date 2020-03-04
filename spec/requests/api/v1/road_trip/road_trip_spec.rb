a3ceb73cb6726cf07e723ca49273efd6eb7302c8
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-03 22:50:15 -0700

    Add road_trip spec

---
+++ spec/requests/api/v1/road_trip/road_trip_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Road Trip' do
+  it 'can find trip details', :vcr do
+    params = {
+      origin: "Denver, CO",
+      destination: "Pueblo, CO"
+    }
+
+    post "/api/v1/road_trip", params: params
+
+    trip_response = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+    expect(trip_response[:origin][:city]).to eq("Denver")
+    expect(trip_response[:destination][:city]).to eq("Pueblo")
+    expect(trip_response[:data][:attributes].count).to eq(4)
+    expect(trip_response[:data][:attributes][:distance].count).to eq(2)
+  end
+end
