02f7b29be3215079fe94c065978dc4c364f697be
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-03 20:39:05 -0700

    remove final assessment methods

--- app/services/location_fetcher.rb
+++ app/services/location_fetcher.rb
@@ -17,16 +17,4 @@ class LocationFetcher
     parse = JSON.parse(response.body, symbolize_names: true)
     Location.save_location(parse)
   end
-
-  def get_distance(start, destination)
-    conn = connection
-    response = conn.get("maps/api/directions/json?origin=#{start.latitude},#{start.longitude}
-                        &destination=#{destination.latitude},#{destination.longitude}&key=#{ENV['GOOGLE_API']}")
-    distance_parse(response)
-  end
-
-  def distance_parse(response)
-    parse = JSON.parse(response.body, symbolize_names: true)
-    Trip.new(parse)
-  end
 end
