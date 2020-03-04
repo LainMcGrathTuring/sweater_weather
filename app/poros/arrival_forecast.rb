d119c3c622b60c0ea1d2d56610266945580060b8
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-04 10:26:34 -0700

    Add feels_like attribute

--- app/poros/arrival_forecast.rb
+++ app/poros/arrival_forecast.rb
@@ -1,10 +1,12 @@
 class ArrivalForecast
 
   attr_reader :temperature,
-              :summary
+              :summary,
+              :feels_like
 
   def initialize(data)
-    @temperature = data[:temperature]
     @summary = data[:summary]
+    @temperature = data[:temperature]
+    @feels_like = data[:apparentTemperature]
   end
 end
