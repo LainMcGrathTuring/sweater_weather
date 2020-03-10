f5789436eef1a014429906c2a6c6bafd560f3167
Lain McGratH <lain.mcgrath@gusto.com>
2020-03-10 14:06:34 -0600

    change VCR configuration

--- spec/requests/api/v1/road_trip/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip/road_trip_spec.rb
@@ -2,26 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Road Trip' do
   it 'can find trip details', :vcr do
-    params = {
-      origin: "Denver, CO",
-      destination: "Pueblo, CO"
-    }
+    VCR.use_cassette('example', :record => :new_episodes) do
+      params = {
+        origin: "Denver, CO",
+        destination: "Pueblo, CO"
+      }
 
-    post "/api/v1/road_trip", params: params
+      post "/api/v1/road_trip", params: params
 
-    trip_response = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
-    expect(trip_response[:origin][:city]).to eq("Denver")
-    expect(trip_response[:destination][:city]).to eq("Pueblo")
-    expect(trip_response.count).to eq(4)
+      trip_response = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+      expect(trip_response[:origin][:city]).to eq("Denver")
+      expect(trip_response[:destination][:city]).to eq("Pueblo")
+      expect(trip_response.count).to eq(4)
 
-    expect(trip_response[:distance].count).to eq(2)
-    expect(trip_response[:distance]).to have_key(:travel_time)
-    expect(trip_response[:distance]).to have_key(:travel_time_in_seconds)
+      expect(trip_response[:distance].count).to eq(2)
+      expect(trip_response[:distance]).to have_key(:travel_time)
+      expect(trip_response[:distance]).to have_key(:travel_time_in_seconds)
 
-    expect(trip_response[:arrival_forecast].count).to eq(3)
-    expect(trip_response[:arrival_forecast]).to have_key(:summary)
-    expect(trip_response[:arrival_forecast]).to have_key(:temperature)
-    expect(trip_response[:arrival_forecast]).to have_key(:feels_like)
+      expect(trip_response[:arrival_forecast].count).to eq(3)
+      expect(trip_response[:arrival_forecast]).to have_key(:summary)
+      expect(trip_response[:arrival_forecast]).to have_key(:temperature)
+      expect(trip_response[:arrival_forecast]).to have_key(:feels_like)
+    end
   end
 
   it 'fails gracefully if no routes are found', :vcr do
